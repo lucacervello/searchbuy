553aa6cdaeb923f5757c5272af282711b9d1c50d
lucacervello <luca.cervello@onebip.com>
2019-02-10 16:03:09 +0100

    delete autogenerated files

--- resources/docs/auto/requirements.el
+++ resources/docs/auto/requirements.el
@@ -5,12 +5,12 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -30,38 +30,38 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org56c52da"
-    "sec:orgfff8088"
-    "sec:org2ce0bcc"
-    "sec:org9e04b14"
-    "sec:org6ec5006"
-    "sec:orga95856c"
-    "sec:org521fd37"
-    "sec:orgb28f336"
-    "sec:org5251284"
-    "sec:org2ae61ec"
-    "sec:org2a42432"
-    "sec:org4ed0fe2"
-    "sec:org2ca3b55"
-    "sec:orge29cabb"
-    "sec:org0e82d8a"
-    "sec:org2ba6c75"
-    "sec:orgb6f424d"
-    "sec:org5fccb14"
-    "sec:org930a3f8"
-    "sec:orgc517a11"
-    "sec:orgfe9d1ad"
-    "sec:org58b03c4"
-    "sec:orgaa1579a"
-    "sec:org36e0b6c"
-    "sec:org9f6cdc6"
-    "sec:orge7abb26"
-    "sec:org03063f3"
-    "sec:org3bf4845"
-    "sec:org04c30f2"
-    "sec:orgf9cddaa"
-    "sec:org396f962"
-    "sec:org90e72ff"
-    "sec:orgf7e03a6"))
+    "sec:org0f30cfa"
+    "sec:org1c01714"
+    "sec:org021c3d6"
+    "sec:org30690f9"
+    "sec:org0661651"
+    "sec:org3a47fa1"
+    "sec:org99672ec"
+    "sec:orgd58623e"
+    "sec:org9985267"
+    "sec:orgdc0146d"
+    "sec:orgf454c8e"
+    "sec:org1018240"
+    "sec:orgda4abaf"
+    "sec:orga22b9a4"
+    "sec:orgcce5ca3"
+    "sec:org3619ef9"
+    "sec:org7e86b86"
+    "sec:org677b0ba"
+    "sec:org75b7276"
+    "sec:orga70236e"
+    "sec:org53b2dfd"
+    "sec:orgb4fbd94"
+    "sec:orgaeaf6dd"
+    "sec:orgc63c060"
+    "sec:org5db38b9"
+    "sec:org5fc7e1f"
+    "sec:org5bc1f52"
+    "sec:orged79b8b"
+    "sec:org15592fc"
+    "sec:org1d8d64e"
+    "sec:orgda2dfad"
+    "sec:org7cc5376"
+    "sec:org638b699"))
  :latex)
 
